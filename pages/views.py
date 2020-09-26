6a02ff015de2350eec6bb27832629ea6bb60704e
nancyruya <nancyruya@gmail.com>
2020-09-25 23:24:12 -0400

    search submit form

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from listings.choices import bedroom_choices, state_choices
+from listings.choices import bedroom_choices, state_choices 
 
 from listings.models import Listing
 from realtors.models import Realtor
