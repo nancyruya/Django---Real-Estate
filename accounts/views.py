56e0a54d7058dc92755a9f9c7a93f5e06504304e
nancyruya <nancyruya@gmail.com>
2020-09-27 10:02:12 -0400

    check password if match; had issue with MultiValueDictKeyError(key)

--- accounts/views.py
+++ accounts/views.py
@@ -3,8 +3,20 @@ from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'Testing error message')
-        return redirect('register')
+        # Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        #check if passwords match
+        if password == password2:
+            return 
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
     else: 
         return render(request, 'accounts/register.html')
 
