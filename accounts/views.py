ae78135d56b05c5383d8fcd4f350745f74498501
nancyruya <nancyruya@gmail.com>
2020-09-27 10:25:11 -0400

    registered user

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.contrib.auth.models import User
 
 def register(request):
@@ -24,7 +24,14 @@ def register(request):
                     return redirect('register')
                 else:
                     #looks good
-                    return
+                    user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+                    #login after register
+                    #auth.login(request, user)
+                    #messages.success(request, 'You are now logged in')
+                    #return redirect('index')
+                    user.save();
+                    messages.success(request, 'You are now registered and can log in')
+                    return redirect('login')
         else:
             messages.error(request, 'Passwords do not match')
             return redirect('register')
